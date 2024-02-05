1dbeb15102149bfd44ff349c58b51d62369899fe
Ubinquitous <ubinquitous1@gmail.com>
2024-02-05 13:28:44 +0900

    set honorific token counter

---
+++ app/utils/honorific_token_counter.py
@@ -0,0 +1,11 @@
+import re
+from konlpy.tag import Komoran
+
+komoran = Komoran()
+hon_tokens = [word.rstrip('\n') for word in open('app/ml/' + 'komoran_honorific_token.txt', 'r',encoding='utf-8')]
+
+def honorific_token_counter(text):
+    cnt = 0
+    for i in komoran.pos(text):
+        if str(i) in hon_tokens: cnt += 1
+    return cnt
